ff9bac22854da7f79c8ffcc0b63eefdf474ec020
Venkatesh-vis <venkateshvishwanadula257@gmail.com>
2024-05-02 13:07:29 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "./App.css";
 
-import Mynav from "./components/Navbar/navbar";
-import Data from "./components/dta/data";
-import Foot from "./components/footer/footer";
-import Car from "./components/Carousel/carousel";
 
 function App() {
   return (
     <div>
-      <Mynav />
-      <Car/>
-      <Data></Data>
-      <Foot/>
+      
     </div>
   );
 }
