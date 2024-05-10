5c1889a10b86f55e724f29cc3b1e072711a5d396
Venkatesh-vis <venkateshvishwanadula257@gmail.com>
2024-05-10 12:49:08 +0530

    msg

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,34 +1,47 @@
-import { useState } from "react"
+import { useState } from "react";
 
+const Todo = () => {
+  const [task, newtask] = useState([]);
 
-const Todo = ()=>{
-    const [task,newtask]=useState([])
+  const addtask = () => {
+    const newTask = prompt("enter your task");
+    newtask((task) => [...task, newTask]);
+    console.log(task);
+  };
+  const clearAllTasks = () => {
+    newtask([]);
+  };
+  const del = (i) => {
+    task.map((t, ind) => {
+      if (i !== ind) {
+        return t
+      }
+    });
+  };
+  const update = (ind) => {
+    const updatedTask = prompt("enter updated task");
+    newtask(task);
+  };
 
-    const addtask=()=>{
-        const newTask = prompt("enter your task")
-        newtask(task=>[...task,newTask])
-        console.log(task);
-    }
-    const clearAllTasks=()=>{
-        newtask([])
-    }
-    
-    return(
-        <>
-        {
-            task.map((task)=>{
-                return(
-                    <div>
-                    <h1>{task}</h1>
-                    <button>Delete</button>
-                    </div>
-                )
-            })
-        }
+  return (
+    <>
+      <div>
         <button onClick={addtask}>Add Task</button>
         <button onClick={clearAllTasks}>Clear All Tasks</button>
-        </>
-    )
-}
+      </div>
+      <>
+        {task.map((task, index) => {
+          return (
+            <div>
+              <h1>{task}</h1>
+              <button onClick={() => del(index)}>Delete</button>
+              <button onClick={() => update(index)}>Update</button>
+            </div>
+          );
+        })}
+      </>
+    </>
+  );
+};
 
-export default Todo
+export default Todo;
