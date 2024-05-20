f013c82ff96f71489e19b8938060aa41bd9e2deb
Venkatesh-vis <venkateshvishwanadula257@gmail.com>
2024-05-20 15:03:16 +0530

    fetching each product

--- src/components/eachproduct/eachproduct.js
+++ src/components/eachproduct/eachproduct.js
@@ -1,44 +1,67 @@
-import { useEffect, useState } from "react"
-import Navb from "../otherComponents/Navbar"
-import { useParams } from "react-router-dom"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import Navb from "../otherComponents/Navbar";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Spinner from "react-bootstrap/Spinner";
+import ListGroup from "react-bootstrap/ListGroup";
 
+const EachProd = () => {
+  const { id } = useParams();
 
+  console.log(id);
 
-const EachProd = ()=> {
+  const [eachprod, setEachprod] = useState({});
 
-    const {id}=useParams()
+  const { title, description, price, brand, category, thumbnail } = eachprod;
 
-    console.log(id);
+  useEffect(() => {
+    fetchProd();
+  }, [id]);
 
-    const[eachprod,setEachprod]= useState({})
+  const fetchProd = async () => {
+    const res = await axios.get(`https://dummyjson.com/products/${id}`);
+    setEachprod(res.data);
+  };
+  console.log(eachprod);
 
-    useEffect(()=>{
-        fetchProd()
-    },[id])
+  return (
+    <>
+      <Navb />
+      <>
+        {Object.keys(eachprod).length > 0 ? (
+          <Card className="text-center">
+            <Card.Body>
+              <Card.Img
+                variant="top"
+                src={thumbnail}
+                height={550}
+                width={250}
+              />
 
-    const fetchProd = async ()=> {
-        const res = await axios.get(`https://dummyjson.com/products/${id}`)
-        setEachprod(res.data)
-        
-    }
-    console.log(eachprod);
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>{description}</Card.Text>
+              <Card.Text>{brand}</Card.Text>
+              <Card.Text>{category}</Card.Text>
+              <Card.Text>{price}</Card.Text>
+              <Button variant="outline-success">Add to Cart</Button>
+            </Card.Body>
+          </Card>
+        ) : (
+          <Spinner
+            style={{
+              height: "70px",
+              width: "70px",
+              margin: "250px 0px 0px 700px",
+            }}
+            animation="grow"
+            variant="success"
+          />
+        )}
+      </>
+    </>
+  );
+};
 
-    return(
-        <>
-        <Navb/>
-        <>
-        {
-            (Object.keys(eachprod).length>0) ?
-
-            <h1>{eachprod.title}</h1>
-            :
-            <h5>loading...</h5>
-        }
-        </>
-
-        </>
-    )
-}
-
-export default EachProd
+export default EachProd;
