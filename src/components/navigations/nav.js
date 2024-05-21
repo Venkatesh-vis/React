9d3c214e2d92ce823e282327c07d78e4d6d5dda5
Venkatesh-vis <venkateshvishwanadula257@gmail.com>
2024-05-21 15:15:13 +0530

    contextapi

--- src/components/navigations/nav.js
+++ src/components/navigations/nav.js
@@ -3,19 +3,30 @@ import Prods from "../home/home"
 import About from "../about/about"
 import Contact from "../Contact/contact"
 import EachProd from "../eachproduct/eachproduct"
+import Cart from "../Cart/cart"
+import { createContext, useState } from "react"
+
+
+export const Item = createContext()
 
 const Navs = ()=>{
+
+    const[eachitem,updateEachItem]=useState({})
+
     return(
         <>
+        <Item.Provider value={eachitem}>
         <BrowserRouter>
         <Routes>
             <Route path='/' Component={Prods}/>
             <Route path='/About' Component={About}/>
             <Route path="/Contact" Component={Contact}/>
             <Route path="/:Product/:id" Component={EachProd}/>
+            <Route path='/Cart' Component={Cart}></Route>
 
         </Routes>
         </BrowserRouter>
+        </Item.Provider>
         </>
     )
 }
--- src/components/otherComponents/Navbar.js
+++ src/components/otherComponents/Navbar.js
@@ -2,6 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const Navb = ()=>{
    const linkStyle = {textDecoration:"none",color:"white"   }
@@ -14,7 +17,13 @@ const Navb = ()=>{
             <Nav.Link><Link style={linkStyle} to={'/About'}>About</Link></Nav.Link>
             <Nav.Link ><Link style={linkStyle} to={'/Contact'}>Contact</Link></Nav.Link>
           </Nav>
+          <Row>
+        <Col xs="auto">
+            <Link style={linkStyle} to={'/Cart'}><Button variant="secondary">Cart</Button></Link>
+          </Col>
+        </Row>
         </Container>
+       
       </Navbar>
         </>
     )
